9c3e794e41950d8d2e88f4118010f841366d70ae
Qwerty9957 <232464434+Qwerty9957@users.noreply.github.com>
2026-03-12 18:15:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ build.js
@@ -0,0 +1,21 @@
+const fs = require("fs/promises");
+const extract = require('extract-zip');
+
+async function setup() {
+    try {
+        await fs.access("static/")
+        console.log("Anura already installed")
+    } catch(e) {
+        console.log("Anura static not installed, downloading")
+        await fs.writeFile("static.zip", Buffer.from(await (await (fetch("https://github.com/MercuryWorkshop/anuraOS/releases/download/latest/anura.zip"))).arrayBuffer())); // Insanity
+        await fs.mkdir("static");
+        console.log("Extracting to " + __dirname + "/static")
+        await extract("static.zip", { dir: __dirname + "/static" });
+
+        const config = JSON.parse(await fs.readFile(__dirname + "/static/config.json", "utf-8"));
+        // config chanegs
+        config.defaultsettings["wisp-url"] = "wss://anura.pro/"; // Wisp server in lieu of vercel supporting one
+        fs.writeFile(__dirname + "/static/config.json", JSON.stringify(config)); // save config
+    }
+}
+setup();
